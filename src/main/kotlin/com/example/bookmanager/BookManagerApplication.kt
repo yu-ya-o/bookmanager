16451e3244081601b0b42bbd6544f039f1739dc6
yu-ya-o <onoyuya1125@gmail.com>
2025-06-28 22:29:48 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/bookmanager/BookManagerApplication.kt
@@ -0,0 +1,12 @@
+package com.example.bookmanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<BookManagerApplication>(*args)
+	println("Hello, World!")
+}
